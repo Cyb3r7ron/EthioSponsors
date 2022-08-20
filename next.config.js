949e19fe36e287e5fcebb4bd3def37a9a7f549f6
Cyb3r7ron <kiduskyle@gmail.com>
2022-08-20 15:30:00 -0400

    last commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: [
+      'yt3.ggpht.com',
+      'lh3.googleusercontent.com',
+      'cdn.pixabay.com',
+      'p16-amd-va.tiktokcdn.com',
+      'image.shutterstock.com'
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ utils/constants.tsx
@@ -0,0 +1,41 @@
+import { GiCyberEye, GiElectricalResistance,GiPlantSeed, GiProcessor, GiStarFormation, GiTakeMyMoney } from 'react-icons/gi';
+import { FaUserFriends, FaTools } from 'react-icons/fa';
+
+export const topics = [
+  {
+    name: 'AgriTech',
+    icon: <GiPlantSeed />,
+  },
+  {
+    name: 'CyberSec',
+    icon: <GiCyberEye />,
+  },
+  {
+    name: 'FinTech',
+    icon: <GiTakeMyMoney />,
+  },
+  {
+    name: 'Social',
+    icon: <FaUserFriends />,
+  },
+  {
+    name: 'Artificial Intelligence',
+    icon: <GiProcessor />,
+  },
+  {
+    name: 'Mechanics',
+    icon: <FaTools />,
+  },
+  {
+    name: 'Electronics',
+    icon: <GiElectricalResistance />,
+  },
+  {
+    name: 'Special',
+    icon: <GiStarFormation />,
+  },
+];
+
+export const footerList1 = ['']
+//export const footerList2 = [ 'TikTik for Good','Advertise','Developers','Transparency','TikTik Rewards' ]
+export const footerList3 = ['About','Terms', 'Privacy', 'Community Guidelines' ]
